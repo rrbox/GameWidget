15c990b40c2e7da75ba75a53c08e6e1ba6cbbd3d
rrbox <87851278+rrbox@users.noreply.github.com>
2022-07-24 00:44:59 +0900

    [add] SKNode 用モディファイアを持つプロトコルを作成

--- Sources/GameWidget/Widget.swift
+++ Sources/GameWidget/Widget.swift
@@ -11,7 +11,71 @@ import GameplayKit
 
 public protocol Widget {
     func node() -> SKNode
+    func addTo(parent list: inout [SKNode])
 }
+
+public extension Widget {
+    func addTo(parent list: inout [SKNode]) {
+        list.append(self.node())
+    }
+}
+
+public protocol MoveableItem: Widget {
+    var position: CGPoint { get set }
+}
+
+public extension MoveableItem {
+    func position(_ value: CGPoint) -> Self {
+        var result = self
+        result.position = value
+        return result
+    }
+}
+
+public protocol RotatableItem: Widget {
+    var zRotation: CGFloat { get set }
+}
+
+public extension RotatableItem {
+    
+    func zRotation(_ value: CGFloat) -> Self {
+        var result = self
+        result.zRotation = value
+        return result
+    }
+    
+}
+
+public protocol ScalableItem: Widget {
+    var xScale: CGFloat { get set }
+    var yScale: CGFloat { get set }
+}
+
+public extension ScalableItem {
+    
+    func scale(_ value: CGFloat) -> Self {
+        var result = self
+        result.xScale = value
+        result.yScale = value
+        return result
+    }
+    
+    func xScale(_ value: CGFloat) -> Self {
+        var result = self
+        result.xScale = value
+        return result
+    }
+    
+    func yScale(_ value: CGFloat) -> Self {
+        var result = self
+        result.yScale = value
+        return result
+    }
+    
+}
+
+public typealias NodeWidget = MoveableItem & RotatableItem & ScalableItem
+
 
 @resultBuilder public struct UIFunction {
     public static func buildBlock<Part: Widget>(_ components: Part...) -> [Part] {
