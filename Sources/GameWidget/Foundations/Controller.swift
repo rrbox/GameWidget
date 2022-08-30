287a4741c956006cc074a5228d4c43ccf2f9d258
rrbox <87851278+rrbox@users.noreply.github.com>
2022-08-30 22:00:38 +0900

    [add] Controller area

--- Sources/GameWidget/Foundations/Controller.swift
+++ Sources/GameWidget/Foundations/Controller.swift
@@ -6,3 +6,24 @@
 //
 
 import GameplayKit
+
+/// クリックするとコントローラーが表示される領域
+class ControllerArea: SKSpriteNode {
+    override var isUserInteractionEnabled: Bool {
+        get { true }
+        set {}
+    }
+    
+    override func mouseDown(with event: NSEvent) {
+        
+    }
+    override func mouseDragged(with event: NSEvent) {
+        
+    }
+    override func mouseUp(with event: NSEvent) {
+        
+    }
+    
+}
+
+
