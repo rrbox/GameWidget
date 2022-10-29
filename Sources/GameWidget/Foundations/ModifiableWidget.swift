23b92a6d90303def5dd32fb939f5dd7c81178f1d
rrbox <87851278+rrbox@users.noreply.github.com>
2022-10-29 23:14:06 +0900

    [add] ModifiableWidget を追加しました.

---
+++ Sources/GameWidget/Foundations/ModifiableWidget.swift
@@ -0,0 +1,30 @@
+//
+//  ModifiableWidget.swift
+//  
+//
+//  Created by rrbox on 2022/10/29.
+//
+
+import SpriteKit
+
+public struct ModifiableWidget<Body: Widget, Builder: ContextBuilder>: Widget where Body.Context == Builder.Mod.Context {
+    public func node(context: Body.Context) -> SKNode {
+        fatalError("\(context)")
+    }
+    
+    public func node() -> SKNode {
+        var context = Context()
+        self.builder.mod(context: &context)
+        return body.node(context: context)
+    }
+    
+    public typealias Context = Body.Context
+    public typealias Next<T: Modifier> = ModifiableWidget<Body, Builder.Next<T>> where T.Context == Body.Context
+    
+    var body: Body
+    var builder: Builder
+    
+    func modifier<T: Modifier>(mod: T) -> Next<T> {
+        .init(body: self.body, builder: self.builder.modifiered(mod: mod))
+    }
+}
