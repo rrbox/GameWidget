e9a2d4b7ad5927e57b1b5b8d87f8d19fe003e7a4
rrbox <87851278+rrbox@users.noreply.github.com>
2022-10-29 23:17:41 +0900

    [add] Empty を追加しました. Widget に Modifier を連結させる際の起点となる構造体です.

--- Sources/GameWidget/Foundations/ModifiableWidget.swift
+++ Sources/GameWidget/Foundations/ModifiableWidget.swift
@@ -28,3 +28,18 @@ public struct ModifiableWidget<Body: Widget, Builder: ContextBuilder>: Widget wh
         .init(body: self.body, builder: self.builder.modifiered(mod: mod))
     }
 }
+
+public struct Empty<Context: ContextProtocol>: Modifier {
+    public func mod(context: inout Context) {
+        
+    }
+    
+}
+
+public extension Widget {
+    typealias Modifiable = ModifiableWidget<Self, SingleModifierBuilder<Empty<Context>>>
+    
+    var modifiable: Modifiable {
+        .init(body: self, builder: SingleModifierBuilder(modData: Empty()))
+    }
+}
