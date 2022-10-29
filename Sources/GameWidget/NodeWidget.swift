14982b26982d697a6c5ffc380d4b7598c9920255
rrbox <87851278+rrbox@users.noreply.github.com>
2022-10-29 23:30:30 +0900

    [move] Node widget を定義するプロトコルを移動しました.

--- Sources/GameWidget/NodeWidget.swift
+++ Sources/GameWidget/NodeWidget.swift
@@ -7,6 +7,60 @@
 
 import SpriteKit
 
+public protocol MoveableItem: Widget {
+    var position: CGPoint { get set }
+}
+
+public extension MoveableItem {
+    func position(_ value: CGPoint) -> Self {
+        var result = self
+        result.position = value
+        return result
+    }
+}
+
+public protocol RotatableItem: Widget {
+    var zRotation: CGFloat { get set }
+}
+
+public extension RotatableItem {
+    func zRotation(_ value: CGFloat) -> Self {
+        var result = self
+        result.zRotation = value
+        return result
+    }
+    
+}
+
+public protocol ScalableItem: Widget {
+    var xScale: CGFloat { get set }
+    var yScale: CGFloat { get set }
+}
+
+public extension ScalableItem {
+    func scale(_ value: CGFloat) -> Self {
+        var result = self
+        result.xScale = value
+        result.yScale = value
+        return result
+    }
+    
+    func xScale(_ value: CGFloat) -> Self {
+        var result = self
+        result.xScale = value
+        return result
+    }
+    
+    func yScale(_ value: CGFloat) -> Self {
+        var result = self
+        result.yScale = value
+        return result
+    }
+    
+}
+
+public typealias NodeWidget = MoveableItem & RotatableItem & ScalableItem
+
 /// 10 個以下の widget を一つの widget としてまとめます. 座標, スケール, 回転を内包するコンテンツと共に調整することができます.
 /// - note: 数値をもつため, 40バイトのメモリを必要とします.
 public struct Node<Content: WidgetList>: NodeWidget, WidgetList {
