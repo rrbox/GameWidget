f2822c1c7dfb491413c101684e296e3a996a784e
rrbox <87851278+rrbox@users.noreply.github.com>
2022-10-29 22:27:52 +0900

    [add] ContextBuilder に `context()` メソッドを追加しました. `context()` メソッドは編集済み `Context` を返します.

--- Sources/GameWidget/Foundations/ContextBuilder.swift
+++ Sources/GameWidget/Foundations/ContextBuilder.swift
@@ -11,3 +11,12 @@ public protocol ContextBuilder {
     var modData: Mod { get set }
     func mod(context: inout Mod.Context)
 }
+
+public extension ContextBuilder {
+    func context() -> Mod.Context {
+        var result = Mod.Context()
+        self.mod(context: &result)
+        return result
+    }
+    
+}
