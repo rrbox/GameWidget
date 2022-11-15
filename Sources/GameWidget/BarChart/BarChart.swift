7942f1a39660f166327e2bf6fcf093ab16702862
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-15 23:43:55 +0900

    [add] HorizontalSingleBarChartContext を作成しました.

--- Sources/GameWidget/BarChart/BarChart.swift
+++ Sources/GameWidget/BarChart/BarChart.swift
@@ -33,65 +33,57 @@ public struct HorizontalSingleBarChart {
     }
     
     var name: ID
-    var position = CGPoint.zero
-    var width = CGFloat(10)
-    var length = CGFloat(100)
-    var alignment = HorizontalBarChartAlignmentMode.right
-    var color = SKColor.systemGreen
-    var backgroundColor = SKColor.gray
     
-    @discardableResult public func width(_ value: CGFloat) -> Self {
-        var result = self
-        result.width = value
-        return result
-    }
     
-    @discardableResult public func length(_ value: CGFloat) -> Self {
-        var result = self
-        result.length = value
-        return result
-    }
-    
-    @discardableResult public func arignment(_ value: HorizontalBarChartAlignmentMode) -> Self {
-        var result = self
-        result.alignment = value
-        return result
-    }
-    
-    @discardableResult public func position(_ value: CGPoint) -> Self {
-        var result = self
-        result.position = value
-        return result
-    }
-    
-    @discardableResult public func color(_ value: SKColor) -> Self {
-        var result = self
-        result.color = value
-        return result
-    }
-    
-    @discardableResult public func backgroundColor(_ value: SKColor) -> Self {
-        var result = self
-        result.backgroundColor = value
-        return result
-    }
+//    @discardableResult public func width(_ value: CGFloat) -> Self {
+//        var result = self
+//        result.width = value
+//        return result
+//    }
+//
+//    @discardableResult public func length(_ value: CGFloat) -> Self {
+//        var result = self
+//        result.length = value
+//        return result
+//    }
+//
+//    @discardableResult public func arignment(_ value: HorizontalBarChartAlignmentMode) -> Self {
+//        var result = self
+//        result.alignment = value
+//        return result
+//    }
+//
+//    @discardableResult public func position(_ value: CGPoint) -> Self {
+//        var result = self
+//        result.position = value
+//        return result
+//    }
+//
+//    @discardableResult public func color(_ value: SKColor) -> Self {
+//        var result = self
+//        result.color = value
+//        return result
+//    }
+//
+//    @discardableResult public func backgroundColor(_ value: SKColor) -> Self {
+//        var result = self
+//        result.backgroundColor = value
+//        return result
+//    }
     
 }
 
 extension HorizontalSingleBarChart: Widget {
-    public typealias Context = Never
+    public typealias Context = HorizontalSingleBarChartContext
     
-    public func node(context: Never) -> SKNode {
-        
-    }
-    
-    public func node() -> SKNode {
-        let result = SKSpriteNode(color: self.backgroundColor, size: CGSize(width: self.length, height: self.width))
-        let bar = SKSpriteNode(color: self.color, size: CGSize(width: self.length, height: self.width))
-        result.anchorPoint = self.alignment.anchorPoint()
-        bar.anchorPoint = self.alignment.anchorPoint()
-        result.position = self.position
+    public func node(context: HorizontalSingleBarChartContext) -> SKNode {
+        let result = SKSpriteNode(color: context.backgroundColor, size: CGSize(width: context.length, height: context.width))
+        let bar = SKSpriteNode(color: context.color, size: CGSize(width: context.length, height: context.width))
+        result.anchorPoint = context.alignment.anchorPoint()
+        bar.anchorPoint = context.alignment.anchorPoint()
+        result.position = context.position
         result.addChild(bar)
         return result
     }
+    
 }
--- Sources/GameWidget/BarChart/BarChartModifiers.swift
+++ Sources/GameWidget/BarChart/BarChartModifiers.swift
@@ -5,6 +5,85 @@
 //  Created by rrbox on 2022/11/15.
 //
 
-public struct BarChartContext {
+import SpriteKit
+
+public struct HorizontalSingleBarChartContext: PositionContextProtocol {
+    public init() {}
     
+    public var position = CGPoint.zero
+    var width = CGFloat(10)
+    var length = CGFloat(100)
+    var alignment = HorizontalBarChartAlignmentMode.right
+    var color = SKColor.systemGreen
+    var backgroundColor = SKColor.gray
+}
+
+public enum HorizontalSingleBarChartModiifers {
+    public struct Width: Modifier {
+        public typealias Context = HorizontalSingleBarChartContext
+        var value: CGFloat
+        
+        public func mod(context: inout HorizontalSingleBarChartContext) {
+            context.width = self.value
+        }
+    }
+    
+    public struct Length: Modifier {
+        public typealias Context = HorizontalSingleBarChartContext
+        var value: CGFloat
+        
+        public func mod(context: inout HorizontalSingleBarChartContext) {
+            context.length = self.value
+        }
+    }
+    
+    public struct Alignment: Modifier {
+        public typealias Context = HorizontalSingleBarChartContext
+        var value: HorizontalBarChartAlignmentMode
+        
+        public func mod(context: inout HorizontalSingleBarChartContext) {
+            context.alignment = self.value
+        }
+    }
+    
+    public struct Color: Modifier {
+        public typealias Context = HorizontalSingleBarChartContext
+        var value: SKColor
+        
+        public func mod(context: inout HorizontalSingleBarChartContext) {
+            context.color = self.value
+        }
+    }
+    
+    public struct BackgroundColor: Modifier {
+        public typealias Context = HorizontalSingleBarChartContext
+        var value: SKColor
+        
+        public func mod(context: inout HorizontalSingleBarChartContext) {
+            context.backgroundColor = self.value
+        }
+    }
+}
+
+public extension ModifiableWidget where Context == HorizontalSingleBarChartContext {
+    func width(_ value: CGFloat) -> Next<HorizontalSingleBarChartModiifers.Width> {
+        self.modifier(mod: HorizontalSingleBarChartModiifers.Width(value: value))
+    }
+    
+    
+    func length(_ value: CGFloat) -> Next<HorizontalSingleBarChartModiifers.Length> {
+        self.modifier(mod: HorizontalSingleBarChartModiifers.Length(value: value))
+    }
+    
+    func alignment(_ value: HorizontalBarChartAlignmentMode) -> Next<HorizontalSingleBarChartModiifers.Alignment> {
+        self.modifier(mod: HorizontalSingleBarChartModiifers.Alignment(value: value))
+    }
+    
+    func color(_ value: SKColor) -> Next<HorizontalSingleBarChartModiifers.Color> {
+        self.modifier(mod: HorizontalSingleBarChartModiifers.Color(value: value))
+    }
+    
+    func backgroundColor(_ value: SKColor) -> Next<HorizontalSingleBarChartModiifers.Color> {
+        self.modifier(mod: HorizontalSingleBarChartModiifers.Color(value: value))
+    }
 }
