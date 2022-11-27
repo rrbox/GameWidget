2fc6c0dd8e08ed687f7ae56c7480fc883b55507e
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-28 04:09:16 +0900

    add `ControllerArea` の unit test を追加しました.

--- Tests/GameWidgetTests/ControllerTests.swift
+++ Tests/GameWidgetTests/ControllerTests.swift
@@ -6,7 +6,24 @@
 //
 
 import XCTest
+import GameWidget
+import SpriteKit
 
 final class ControllerTests: XCTestCase {
-    
+    func testModifiers() {
+        let controller = ControllerArea(.init("test"))
+            .modifiable
+            .position(CGPoint(x: 1, y: 1))
+            .size(CGSize(width: 32, height: 32))
+            .node()
+        
+        guard let controller = controller as? SKSpriteNode else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertEqual(controller.position, CGPoint(x: 1, y: 1))
+        XCTAssertEqual(controller.size, CGSize(width: 32, height: 32))
+        
+    }
 }
