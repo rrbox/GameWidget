97d5d1de8fdce6966e63fcb876e34c2e0c58d159
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-28 01:49:57 +0900

    add `ControllerContext` を追加しました.

--- Sources/GameWidget/Controller/ControllerModifiers.swift
+++ Sources/GameWidget/Controller/ControllerModifiers.swift
@@ -5,4 +5,12 @@
 //  Created by rrbox on 2022/11/28.
 //
 
-import Foundation
+import CoreGraphics
+
+public struct ControllerContext: PositionContextProtocol,
+                          ResizeableContextProtocol {
+    public var position: CGPoint = .zero
+    public var size: CGSize = CGSize(width: 100, height: 100)
+    
+    public init() {}
+}
