b0bbce76fbe22187bb984159d887f3d9cfb95b41
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-28 04:05:01 +0900

    add `HorizontalBarChart` の unit test を追加しました.

--- Tests/GameWidgetTests/BarChartTests.swift
+++ Tests/GameWidgetTests/BarChartTests.swift
@@ -6,7 +6,34 @@
 //
 
 import XCTest
+import GameWidget
+import SpriteKit
 
 final class BarChartTests: XCTestCase {
-    
+    func testModifiers() {
+        let barChart = HorizontalSingleBarChart(name: .init("test"))
+            .modifiable
+            .position(CGPoint(x: 1, y: 1))
+            .color(.red)
+            .width(90)
+            .length(90)
+            .alignment(.center)
+            .backgroundColor(.blue)
+            .node()
+        
+        guard let barChart = barChart as? SKSpriteNode,
+              let bar = barChart.children[0] as? SKSpriteNode else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertEqual(barChart.position, CGPoint(x: 1, y: 1))
+        XCTAssertEqual(barChart.color.cgColor.components, SKColor.blue.cgColor.components)
+        XCTAssertEqual(barChart.size, CGSize(width: 90, height: 90))
+        
+        XCTAssertEqual(bar.color.cgColor.components, SKColor.red.cgColor.components)
+        XCTAssertEqual(bar.size, CGSize(width: 90, height: 90))
+        XCTAssertEqual(bar.anchorPoint, CGPoint(x: 0.5, y: 0.5))
+        
+    }
 }
