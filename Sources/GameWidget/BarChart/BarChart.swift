ca0c6cb34c15943c2b0f0955741faa8e57748d01
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-28 01:42:30 +0900

    clean 余分なコメントアウトを削除しました.

--- Sources/GameWidget/BarChart/BarChart.swift
+++ Sources/GameWidget/BarChart/BarChart.swift
@@ -34,43 +34,6 @@ public struct HorizontalSingleBarChart {
     
     var name: ID
     
-    
-//    @discardableResult public func width(_ value: CGFloat) -> Self {
-//        var result = self
-//        result.width = value
-//        return result
-//    }
-//
-//    @discardableResult public func length(_ value: CGFloat) -> Self {
-//        var result = self
-//        result.length = value
-//        return result
-//    }
-//
-//    @discardableResult public func arignment(_ value: HorizontalBarChartAlignmentMode) -> Self {
-//        var result = self
-//        result.alignment = value
-//        return result
-//    }
-//
-//    @discardableResult public func position(_ value: CGPoint) -> Self {
-//        var result = self
-//        result.position = value
-//        return result
-//    }
-//
-//    @discardableResult public func color(_ value: SKColor) -> Self {
-//        var result = self
-//        result.color = value
-//        return result
-//    }
-//
-//    @discardableResult public func backgroundColor(_ value: SKColor) -> Self {
-//        var result = self
-//        result.backgroundColor = value
-//        return result
-//    }
-    
 }
 
 extension HorizontalSingleBarChart: Widget {
