0370f5c0590b9de7c458ba21bd944da81889afa6
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-28 22:21:59 +0900

    add BarChart に送信された数値を反映させるシステムを作成しました.

--- Sources/GameWidget/BarChart/BarChartNode.swift
+++ Sources/GameWidget/BarChart/BarChartNode.swift
@@ -1,8 +1,30 @@
 //
-//  File.swift
+//  BarChartNode.swift
 //  
 //
 //  Created by rrbox on 2022/11/28.
 //
 
-import Foundation
+import SpriteKit
+
+class HorizontalBarChartNode: SKSpriteNode {
+    static let center = NotificationCenter()
+    var maxWidth: CGFloat = .zero
+    
+    @objc func receiveValue(notification: Notification) {
+        self.size.width = (notification.userInfo?["value"] as! CGFloat) * self.maxWidth
+    }
+    
+    @objc func receiveRate(notification: Notification) {
+        self.size.width = (notification.userInfo?["rate"] as! CGFloat) * self.maxWidth
+    }
+    
+    @objc func receiveWithAnimation(notification: Notification) {
+        
+    }
+    
+    func registerTo(center: NotificationCenter, id: HorizontalSingleBarChart.ID) {
+        center.addObserver(self, selector: #selector(receiveValue(notification:)), name: .barChartPostValue(id: id), object: nil)
+        center.addObserver(self, selector: #selector(receiveRate(notification:)), name: .barChartPostRate(id: id), object: nil)
+    }
+}
