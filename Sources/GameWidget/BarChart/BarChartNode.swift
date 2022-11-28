e75313126dd6da385fe3670662f89b64d7cd2975
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-28 22:33:10 +0900

    add BarChart の数値変更をアニメーション付きで表示するメソッドを追加しました.

--- Sources/GameWidget/BarChart/BarChartNode.swift
+++ Sources/GameWidget/BarChart/BarChartNode.swift
@@ -12,19 +12,25 @@ class HorizontalBarChartNode: SKSpriteNode {
     var maxWidth: CGFloat = .zero
     
     @objc func receiveValue(notification: Notification) {
-        self.size.width = (notification.userInfo?["value"] as! CGFloat) * self.maxWidth
+        self.size.width = (notification.userInfo?["value"] as! CGFloat)
     }
     
     @objc func receiveRate(notification: Notification) {
         self.size.width = (notification.userInfo?["rate"] as! CGFloat) * self.maxWidth
     }
     
-    @objc func receiveWithAnimation(notification: Notification) {
-        
+    @objc func receiveValueWithAnimation(notification: Notification) {
+        self.run(.resize(toWidth: (notification.userInfo?["value"] as! CGFloat), duration: 0.5))
+    }
+    
+    @objc func receiveRateWithAnimation(notification: Notification) {
+        self.run(.resize(toWidth: (notification.userInfo?["rate"] as! CGFloat) * self.maxWidth, duration: 0.5))
     }
     
     func registerTo(center: NotificationCenter, id: HorizontalSingleBarChart.ID) {
         center.addObserver(self, selector: #selector(receiveValue(notification:)), name: .barChartPostValue(id: id), object: nil)
         center.addObserver(self, selector: #selector(receiveRate(notification:)), name: .barChartPostRate(id: id), object: nil)
+        center.addObserver(self, selector: #selector(receiveValueWithAnimation(notification:)), name: .barChartPostValueWithAnimation(id: id), object: nil)
+        center.addObserver(self, selector: #selector(receiveRateWithAnimation(notification:)), name: .barChartPostRateWithAnimation(id: id), object: nil)
     }
 }
--- Sources/GameWidget/BarChart/BarChartOutput.swift
+++ Sources/GameWidget/BarChart/BarChartOutput.swift
@@ -14,6 +14,12 @@ extension Notification.Name {
     static func barChartPostValue(id: HorizontalSingleBarChart.ID) -> Self {
         .init("barChartPostValue_\(id)")
     }
+    static func barChartPostRateWithAnimation(id: HorizontalSingleBarChart.ID) -> Self {
+        .init("barChartPostRateWithAnimation_\(id)")
+    }
+    static func barChartPostValueWithAnimation(id: HorizontalSingleBarChart.ID) -> Self {
+        .init("barChartPostValueWithAnimation_\(id)")
+    }
 }
 
 public protocol SingleBarChartDrawable {
@@ -29,5 +35,12 @@ public extension SingleBarChartDrawable {
     }
     func send(rate: CGFloat, id: HorizontalSingleBarChart.ID) {
         HorizontalBarChartNode.center.post(name: .barChartPostRate(id: id), object: nil, userInfo: ["rate": rate])
+    }
+    
+    func sendForAnimation(value: CGFloat, id: HorizontalSingleBarChart.ID) {
+        HorizontalBarChartNode.center.post(name: .barChartPostValueWithAnimation(id: id), object: nil, userInfo: ["value": value])
+    }
+    func sendForAnimation(rate: CGFloat, id: HorizontalSingleBarChart.ID) {
+        HorizontalBarChartNode.center.post(name: .barChartPostRateWithAnimation(id: id), object: nil, userInfo: ["rate": rate])
     }
 }
