1384ec2c6b69db8b03f79478d503361da4800005
rrbox <87851278+rrbox@users.noreply.github.com>
2022-12-02 00:43:52 +0900

    update Group テストを作成しました.
    Group に正しい順番で widget がセットされるかチェックします.

--- Tests/GameWidgetTests/GroupTests.swift
+++ Tests/GameWidgetTests/GroupTests.swift
@@ -6,7 +6,111 @@
 //
 
 import XCTest
+import SpriteKit
+import GameWidget
+
+struct TextNode: Widget {
+    let value: String
+    
+    typealias Context = Never
+    func node(context: Never) -> SKNode {
+        
+    }
+    
+    func node() -> SKNode {
+        let node = SKNode()
+        node.name = self.value
+        return node
+    }
+}
 
 final class GroupTest: XCTestCase {
+    func generateNodesNameReduced<T: WidgetList>(list: Extension<T>) -> String {
+        list.widgetNodes().reduce(into: "") { partialResult, node in
+            guard let name = node.name else {
+                XCTFail()
+                return
+            }
+            partialResult += name
+        }
+    }
     
+    // group に widget が正しい順番で追加されているかチェックします.
+    func testNodeArrayGenerate() {
+        XCTAssertEqual("0", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+        }))
+        XCTAssertEqual("01", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+        }))
+        XCTAssertEqual("012", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+            TextNode(value: "2")
+        }))
+        XCTAssertEqual("0123", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+            TextNode(value: "2")
+            TextNode(value: "3")
+        }))
+        XCTAssertEqual("01234", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+            TextNode(value: "2")
+            TextNode(value: "3")
+            TextNode(value: "4")
+        }))
+        XCTAssertEqual("012345", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+            TextNode(value: "2")
+            TextNode(value: "3")
+            TextNode(value: "4")
+            TextNode(value: "5")
+        }))
+        XCTAssertEqual("0123456", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+            TextNode(value: "2")
+            TextNode(value: "3")
+            TextNode(value: "4")
+            TextNode(value: "5")
+            TextNode(value: "6")
+        }))
+        XCTAssertEqual("01234567", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+            TextNode(value: "2")
+            TextNode(value: "3")
+            TextNode(value: "4")
+            TextNode(value: "5")
+            TextNode(value: "6")
+            TextNode(value: "7")
+        }))
+        XCTAssertEqual("012345678", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+            TextNode(value: "2")
+            TextNode(value: "3")
+            TextNode(value: "4")
+            TextNode(value: "5")
+            TextNode(value: "6")
+            TextNode(value: "7")
+            TextNode(value: "8")
+        }))
+        XCTAssertEqual("0123456789", self.generateNodesNameReduced(list: Extension {
+            TextNode(value: "0")
+            TextNode(value: "1")
+            TextNode(value: "2")
+            TextNode(value: "3")
+            TextNode(value: "4")
+            TextNode(value: "5")
+            TextNode(value: "6")
+            TextNode(value: "7")
+            TextNode(value: "8")
+            TextNode(value: "9")
+        }))
+    }
 }
