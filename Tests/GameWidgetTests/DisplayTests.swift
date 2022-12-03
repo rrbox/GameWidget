b08d420b2d2e8f45ff7b19596078be8b15fa0465
rrbox <87851278+rrbox@users.noreply.github.com>
2022-12-03 19:39:11 +0900

    add Display 用のテストコードを追加しました.

--- Tests/GameWidgetTests/DisplayTests.swift
+++ Tests/GameWidgetTests/DisplayTests.swift
@@ -10,6 +10,20 @@ import GameWidget
 
 final class DisplayTests: XCTestCase {
     func testDisplayGenerate() {
+        let displayNode = Display()
+            .place {
+                TextNode(value: "0")
+                TextNode(value: "1")
+            }
+            .place {
+                TextNode(value: "2")
+                TextNode(value: "3")
+            }
+            .node()
         
+        XCTAssertEqual(displayNode.children.count, 4)
+        XCTAssertEqual(displayNode.children.reduce(into: "", { partialResult, node in
+            partialResult += node.name!
+        }), "0123")
     }
 }
