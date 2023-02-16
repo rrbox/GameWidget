c156c7c329bdb5539a6e1dd567d5ff3687cfb520
rrbox <87851278+rrbox@users.noreply.github.com>
2023-02-16 23:25:50 +0900

    clean Widget 定義 protocol をファイルで分割しました.

---
+++ Sources/GameWidget/Commons/Widget/Widget+PlugIns.swift
@@ -0,0 +1,12 @@
+//
+//  Widget+PlugIns.swift
+//  
+//
+//  Created by rrbox on 2023/02/16.
+//
+
+public extension NodeGenerator where Self: ContextPresentPlugIn, Context: ParameterLessGeneratable {
+    func node() -> Node {
+        self.node(context: Context())
+    }
+}
--- Sources/GameWidget/Commons/Widget/Widget.swift
+++ Sources/GameWidget/Commons/Widget/Widget.swift
@@ -7,12 +7,6 @@
 
 import SpriteKit
 
-public protocol ContextPresentPlugIn {
-    associatedtype Context: WidgetContextType
-    associatedtype Node: SKNode
-    func node(context: Context) -> Node
-}
-
 public protocol NodeGenerator {
     associatedtype Node: SKNode
     func node() -> Node
@@ -29,11 +23,5 @@ public protocol Widget: NodeGenerator, WidgetListElementType {
 public extension WidgetListElementType where Self: NodeGenerator {
     func addTo(buffer: inout [SKNode]) {
         buffer.append(self.node())
-    }
-}
-
-public extension NodeGenerator where Self: ContextPresentPlugIn, Context: ParameterLessGeneratable {
-    func node() -> Node {
-        self.node(context: Context())
     }
 }
