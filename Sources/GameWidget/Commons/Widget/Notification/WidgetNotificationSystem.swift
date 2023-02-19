f3d7155dff7cc2428075b6cb6972d8c1e19c359b
rrbox <87851278+rrbox@users.noreply.github.com>
2023-02-19 13:33:01 +0900

    remove WidgetObserver を削除しました.

--- Sources/GameWidget/Commons/Widget/Notification/WidgetNotificationSystem.swift
+++ Sources/GameWidget/Commons/Widget/Notification/WidgetNotificationSystem.swift
@@ -7,19 +7,10 @@
 
 import Foundation
 
-@objc public protocol WidgetObserver {
-    func registerTo(_ system: WidgetNotificationSystem)
-}
-
 final public class WidgetNotificationSystem: NotificationCenter {
     public static weak var activated: WidgetNotificationSystem?
     
     public func activate() {
         WidgetNotificationSystem.activated = self
-    }
-    
-    public func add(observer: some WidgetObserver) -> Self {
-        observer.registerTo(self)
-        return self
     }
 }
